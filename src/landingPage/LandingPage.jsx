a65dc9276040d1fd449ef3f8658e6f7c6735d69f
naanaaa00 <133928212+naanaaa00@users.noreply.github.com>
2024-04-01 13:56:32 +0700

    Update LandingPage.jsx

--- src/landingPage/LandingPage.jsx
+++ src/landingPage/LandingPage.jsx
@@ -5,11 +5,13 @@ import { P2 } from './components/P2'
 import { P3 } from './components/P3'
 import { P4 } from './components/P4'
 import { Footer } from './components/Footer'
+import { Utama } from './components/Utama'
 
 
 const LandingPage = () => {
   return (
     <div>
+      <Utama/>
       <P2/>
       <P3/>
       <P4/>
