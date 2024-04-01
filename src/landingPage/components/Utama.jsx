069e4c9733ae0d07e5647146b514dd2369ac2f5d
naanaaa00 <133928212+naanaaa00@users.noreply.github.com>
2024-04-01 13:55:37 +0700

    Update Utama.jsx

--- src/landingPage/components/Utama.jsx
+++ src/landingPage/components/Utama.jsx
@@ -1,49 +1,30 @@
-// import React from 'react';
-// import bg from '../assets/utama/Group-64.svg';
+import React from 'react';
+import bg from '../assets/utama/Group-64.svg';
+import { Utama2 } from './Utama2';
 
-// export const Utama = () => {
-//   return (
-//     <div className='h-screen w-full bg-hijau border-2 border-orange '>
-//         <div className='container flex flex-col'>
-//         <div className='flex justify-center items-center w-screen'>
-//             <div className='w-screen flex justify-between items-center px-4 py-2 border-2 border-indigo-600'>
-//                 <span className='font-bold  text-red-700'>LOGO BIR</span>
-//                 <div className='flex space-x-4'>
-//                     <span className='font-bold py-2'>About</span>
-//                     <span className='font-bold py-2'>Contact</span>
-//                     <span className='border-2 rounded-lg border-hijau2 py-2 px-6 font-bold'>Login</span>
-//                     <span className='border-2 rounded-lg border-hijau2 bg-hijau2 py-2 px-6 text-putih font-bold'>Register</span>
-//                 </div>
-//             </div>
-//             </div>
-//             <div className='border-2 border-indigo-600 flex flex-col m-8 justify-center items-center h-85vh'>
-//                 <span className='text-4xl font-bold mb-4'>BUKU INTERAKTIF</span>
-//                 <span className='text-4xl font-bold mb-4'>REMAJA</span>
-//                 <div className='py-8'>
-//                     <span className='border-2 rounded-lg border-hijau2 bg-hijau2 px-4 py-2 text-base font-bold text-putih'>Baca</span>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-//   );
-// };
+export const Utama = () => {
+  return (
+    <div className="">
+      <Utama2 />
+      <div className='bg-hijau flex justify-center items-center h-screen w-full'>
+        <img 
+          src={bg}
+          className='w-full h-full absolute inset-0 object-cover hidden sm:block'
+          alt="background"
+        />
+        <div className=' text-center p-8'>
+          <div className='flex flex-col'>
+            <span className='font-bold text-4xl'>BUKU INTERAKTIF</span>
+            <span className='font-bold text-4xl'>REMAJA</span>
+          </div>
+          <div className="m-8 relative ">
+  <span className="z-99 inline-block px-6 py-1 text-xl font-bold text-white bg-hijau2 border-2 border-hijau2 rounded-lg transition duration-300 hover:bg-hijau hover:text-black">
+    Baca
+  </span>
+</div>
 
-// import React from 'react';
-// import bg from '../assets/utama/Group-64.svg';
-
-// export const Utama = () => {
-//   return (
-//     <div className='w-full bg-hijau flex flex-row justify-between items-center px-4 py-2'>
-//         <div>
-//             <span className='font-bold  text-red-700'>LOGO BIR</span>
-//         </div>
-//         <div className='flex items-end space-x-4'>
-//             <span className='font-bold py-2'>About</span>
-//             <span className='font-bold py-2'>Contact</span>
-//             <span className='border-2 rounded-lg border-hijau2 py-2 px-6 font-bold '>Login</span>
-//             <span className='border-2 rounded-lg border-hijau2 bg-hijau2 py-2 px-6 text-putih font-bold'>Register</span>
-//         </div>
-//     </div>
-//   );
-// };
-
+        </div>
+      </div>
+    </div>
+  );
+};
