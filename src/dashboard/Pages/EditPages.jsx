1186468f52f1c5b98dbefbc8590f490327a863b3
naanaaa00 <133928212+naanaaa00@users.noreply.github.com>
2024-05-03 22:47:47 +0700

    Update EditPages.jsx

--- src/dashboard/Pages/EditPages.jsx
+++ src/dashboard/Pages/EditPages.jsx
@@ -4,9 +4,10 @@ import { Navbar } from '../navbar/Navbar'
 export const EditPages = () => {
   return (
     <div className='  flex flex-col'>
-        <Navbar/>
+      <Navbar/>
+      <div className='self-center h-screen'>
         <div className='border-2 border-indigo-600 h-55vh w-35vw self-center p-8 rounded-3xl md:justify-center my-14 shadow-2xl'>
-        <input type="text" placeholder="Input Id" class="input w-full mb-6" />
+          <input type="text" placeholder="Input Id" class="input w-full mb-6" />
           <textarea className="textarea textarea-warning w-full mb-6" placeholder="Summary"></textarea>
           <div className='p-2 flex flex-col sm:flex-row sm:justify-around'>
             <input type="text" class="file-input file-input-bordered file-input-xs w-full max-w-xs mr-3 mb-6" placeholder='relatedBookId'/>
@@ -16,6 +17,27 @@ export const EditPages = () => {
             <button className='justify-self-center border-2 border-indigo-600 rounded-lg p-2'>Edit!</button>
           </div>
         </div>
-    </div>
+      </div>
+    <div className='h-60vh overflow-x-auto overflow-auto border-2 border-indigo-600 self-center p-8 rounded-3xl md:justify-center my-14 shadow-2xl -mt-14'>
+      <table class="table">
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>storyText</th>
+          <th>image</th>
+          <th>relatedBookId</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr className='hover'>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  </div>
   )
 }
