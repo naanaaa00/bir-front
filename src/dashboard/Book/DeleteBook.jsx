01428852e42f1e96aa24bdbeba72ddbfc0c915cf
naanaaa00 <133928212+naanaaa00@users.noreply.github.com>
2024-05-03 22:40:47 +0700

    Update DeleteBook.jsx
    update deleted book

--- src/dashboard/Book/DeleteBook.jsx
+++ src/dashboard/Book/DeleteBook.jsx
@@ -17,13 +17,26 @@ export const DeleteBook = () => {
   };
   return (
     <div className="flex flex-col">
-      <Navbar />
-      <div className="border-2 border-indigo-600 self-center p-8 rounded-3xl md:justify-center my-14 shadow-2xl">
-        <form action={deleteBook}>
-          <input type="text" placeholder="ID Book" value={id} class="self-start input w-2/4 mr-6" />
-          <button className="-mr-12 border-2 border-indigo-600 rounded-lg p-4  ">Submit!</button>
-        </form>
-      </div>
-    </div>
+    <Navbar />
+    <div className='overflow-x-auto overflow-auto border-2 border-indigo-600 self-center p-8 rounded-3xl md:justify-center my-14 shadow-2xl h-full'>
+      <table class="table">
+      <thead>
+      <tr>
+        <th>bookID</th>
+        <th>tittle</th>
+        <th>summary</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr className=''>
+        <th></th>
+        <td></td>
+        <td></td>
+        <td><button className='hover hover:bg-indigo-600 rounded-lg border-2 border-indigo-600 p-2'>delete</button></td>
+      </tr>
+    </tbody>
+    </table>
+  </div>
+  </div>
   );
 };
