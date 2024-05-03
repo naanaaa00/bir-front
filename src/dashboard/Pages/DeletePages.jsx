6bb68cc9f6014b9222c0282d6138aa5274529980
naanaaa00 <133928212+naanaaa00@users.noreply.github.com>
2024-05-03 22:44:58 +0700

    Update DeletePages.jsx

--- src/dashboard/Pages/DeletePages.jsx
+++ src/dashboard/Pages/DeletePages.jsx
@@ -4,13 +4,27 @@ import { Navbar } from '../navbar/Navbar'
 export const DeletePages = () => {
   return (
     <div className='flex flex-col'>
-        <Navbar />
-       
-        <div className='border-2 border-indigo-600 self-center p-8 rounded-3xl md:justify-center my-14 shadow-2xl'>
-          <input type="text" placeholder="ID Pages" class="input w-2/4 mr-6" />
-          <button className='-mr-12 border-2 border-indigo-600 rounded-lg p-4  '>Submit!</button>
-        </div>
-
+      <Navbar />
+      <div className='overflow-x-auto overflow-auto border-2 border-indigo-600 self-center p-8 rounded-3xl md:justify-center my-14 shadow-2xl h-full'>
+      <table class="table">
+        <thead>
+        <tr>
+          <th>pageID</th>
+          <th>relatedBookId</th>
+          <th>storytext</th>
+        </tr>
+        </thead>
+        <tbody>
+  
+        <tr className=''>
+          <th></th>
+          <td></td>
+          <td></td>
+          <td><button className='hover hover:bg-indigo-600 rounded-lg border-2 border-indigo-600 p-2'>delete</button></td>
+        </tr>
+        </tbody>
+      </table>
+      </div>
     </div>
   )
 }
